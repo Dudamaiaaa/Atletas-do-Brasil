4f973aa91a3f50f2ebdb70a27c6cbf6b8d1bbeff
Dudamaiaaa <dudamaiaaa103@gmail.com>
2025-07-01 20:49:14 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+    titulo : "Rayssa Leal", 
+    descricao : "Jhulia Rayssa Mendes Leal (Imperatriz, 4 de janeiro de 2008) é uma skatista brasileira, vice-campeã olímpica nos Jogos Olímpicos de Verão de 2020 em Tóquio, sendo a mais jovem medalhista olímpica brasileira.",
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+},
+
+    {
+        titulo : "Rebeca Andrade", 
+        descricao : "Rebeca Andrade é uma das maiores ginastas da história do Brasil. Com um talento inegável e uma força de vontade admirável, ela conquistou o coração dos brasileiros e se tornou um símbolo de superação e determinação.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+
+        {
+            titulo : "Beatriz Sousa", 
+            descricao : "Beatriz Souza (Itariri, 20 de maio de 1998) é uma judoca, sargento do exército e campeã olímpica brasileira. Beatriz conquistou a primeira medalha de ouro do Brasil nas Olimpíadas de 2024 em Paris e com isso se tornou a primeira mulher a conquistar uma medalha de ouro Olímpica na história do Esporte Clube Pinheiros.",
+            link : "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+        },
+]; 
+
+
+    
